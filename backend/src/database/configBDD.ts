84107e3c7a16a1a59fe0cbd146166832732a74e6
Luigi-44 <luigi.rondeau@gmail.com>
2025-04-02 14:09:44 +0200

    Connection a la BDD confirmé

---
+++ backend/src/database/configBDD.ts
@@ -0,0 +1,25 @@
+import mysql2 from "mysql2"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DB_PORT } = process.env;
+
+export const connection = mysql2.createConnection({
+  host: process.env.DB_HOST,
+  port: Number.parseInt(DB_PORT as string),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+export const initDB = () => {
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  connection.connect((err: any) => {
+    if (err) {
+      console.error('❌ Erreur de connexion à MySQL :', err);
+      return;
+    }
+    console.log('✅ Connecté à SignetPro');
+  });
+}
--- backend/src/module/app.ts
+++ backend/src/module/app.ts
@@ -1,13 +1,20 @@
 import express from "express";
 // import cors from "cors" //on import npm i cors dans notre back pour "échanger" avec notre front et lui dire, ok tu peux échanger avec l'URL http://localhost:XXXX
+// import connection from "../database/configBDD"
+import { initDB}  from "../database/configBDD"
+
 const app = express(); // Stock toute la puissance d'express dans app
 
+
 const port = 3000;
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+initDB()
+
 app.listen(port, () => {
-  console.log(`💜 Frontend lancé http://localhost:${port}`);
+  console.log(`💜 Backend lancé http://localhost:${port}`);
 });
